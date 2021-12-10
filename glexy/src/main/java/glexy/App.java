84148c69c9c4f858346ffd11ed6734b445ae8044
septsept98 <septianardi053@gmail.com>
2021-12-10 15:57:44 +0700

    new

--- glexy/src/main/java/glexy/App.java
+++ glexy/src/main/java/glexy/App.java
@@ -5,5 +5,6 @@ public class App {
 	public static void main(String[] args) {
 		System.out.println("Masukkan : ");
 		System.out.println("Masukkan Angka : ");
+		System.out.println("Masukkan Huruf : ");
 	}
 }
