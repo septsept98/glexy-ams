5c1d9f403430059accab2d07c3364f0032b3e2d4
septsept98 <septianardi053@gmail.com>
2021-12-13 11:42:49 +0700

    Status Asset

---
+++ glexy/src/main/java/glexy/dao/StatusAssetDaoImpl.java
@@ -0,0 +1,35 @@
+package glexy.dao;
+
+import java.util.List;
+
+import com.lawencon.base.BaseDaoImpl;
+
+import glexy.model.StatusAsset;
+
+public class StatusAssetDaoImpl extends BaseDaoImpl<StatusAsset> implements StatusAssetDao {
+
+	@Override
+	public List<StatusAsset> findAll() throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public StatusAsset findById(String id) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public StatusAsset saveOrUpdate(StatusAsset data) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean deleteById(String id) throws Exception {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
