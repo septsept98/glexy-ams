24a56f4091f9e653eca51f5b2ecd013d47b57d16
septsept98 <septianardi053@gmail.com>
2021-12-13 10:51:40 +0700

    model transaction

---
+++ glexy/src/main/java/glexy/model/TrackAsset.java
@@ -0,0 +1,61 @@
+package glexy.model;
+
+import java.time.LocalDate;
+
+import com.lawencon.base.BaseEntity;
+
+public class TrackAsset extends BaseEntity {
+
+	private static final long serialVersionUID = 1251049900399795603L;
+
+	private String codeAsset;
+	
+	private String nameActivity;
+	
+	private LocalDate dateActivity;
+	
+	private String userId;
+	
+	private String transactionCode;
+
+	public String getCodeAsset() {
+		return codeAsset;
+	}
+
+	public void setCodeAsset(String codeAsset) {
+		this.codeAsset = codeAsset;
+	}
+
+	public String getNameActivity() {
+		return nameActivity;
+	}
+
+	public void setNameActivity(String nameActivity) {
+		this.nameActivity = nameActivity;
+	}
+
+	public LocalDate getDateActivity() {
+		return dateActivity;
+	}
+
+	public void setDateActivity(LocalDate dateActivity) {
+		this.dateActivity = dateActivity;
+	}
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public String getTransactionCode() {
+		return transactionCode;
+	}
+
+	public void setTransactionCode(String transactionCode) {
+		this.transactionCode = transactionCode;
+	}
+	
+}
---
+++ glexy/src/main/java/glexy/model/Transactions.java
@@ -0,0 +1,85 @@
+package glexy.model;
+
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import com.lawencon.base.BaseEntity;
+
+@Entity
+public class Transactions extends BaseEntity{
+	
+	private static final long serialVersionUID = -8411698396500371421L;
+
+	@Column(name = "code_transaction")
+	private String codeTransaction;
+	
+	@Column(name = "checkout_date")
+	private LocalDate checkOutDate;
+	
+	private Integer quantity;
+	
+	@ManyToOne
+	@JoinColumn(name = "employee_id", columnDefinition = "varchar")
+	private Employee employeeId;
+	
+	@ManyToOne
+	@JoinColumn(name = "location_id", columnDefinition = "varchar")
+	private Location locationId;
+	
+	@ManyToOne
+	@JoinColumn(name = "asset_general_id", columnDefinition = "varchar")
+	private Asset assetGeneralId;
+
+	public String getCodeTransaction() {
+		return codeTransaction;
+	}
+
+	public void setCodeTransaction(String codeTransaction) {
+		this.codeTransaction = codeTransaction;
+	}
+
+	public LocalDate getCheckOutDate() {
+		return checkOutDate;
+	}
+
+	public void setCheckOutDate(LocalDate checkOutDate) {
+		this.checkOutDate = checkOutDate;
+	}
+
+	public Integer getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(Integer quantity) {
+		this.quantity = quantity;
+	}
+
+	public Employee getEmployeeId() {
+		return employeeId;
+	}
+
+	public void setEmployeeId(Employee employeeId) {
+		this.employeeId = employeeId;
+	}
+
+	public Location getLocationId() {
+		return locationId;
+	}
+
+	public void setLocationId(Location locationId) {
+		this.locationId = locationId;
+	}
+
+	public Asset getAssetGeneralId() {
+		return assetGeneralId;
+	}
+
+	public void setAssetGeneralId(Asset assetGeneralId) {
+		this.assetGeneralId = assetGeneralId;
+	}
+	
+}
