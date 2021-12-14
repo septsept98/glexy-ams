ee1df0e5e2a3ccc0fee3ebf6504002316b802f64
febri228 <febri.ufairahasan20@gmail.com>
2021-12-14 11:15:18 +0700

    controller febri

--- glexy/src/main/java/com/lawencon/glexy/controller/CompanyController.java
+++ glexy/src/main/java/com/lawencon/glexy/controller/CompanyController.java
@@ -14,44 +14,44 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.lawencon.glexy.model.Asset;
-import com.lawencon.glexy.service.AssetService;
+import com.lawencon.glexy.model.Company;
+import com.lawencon.glexy.service.CompanyService;
 
 @RestController
-@RequestMapping("assets")
-public class AssetController {
+@RequestMapping("companies")
+public class CompanyController {
 	
 	@Autowired
-	private AssetService assetService;
+	private CompanyService companyService;
 	
 	@GetMapping
 	public ResponseEntity<?> getAll() throws Exception {
-		List<Asset> result = assetService.findAll();
+		List<Company> result = companyService.findAll();
 		return new ResponseEntity<>(result, HttpStatus.OK);
 	}
 	
 	@GetMapping("{id}")
 	public ResponseEntity<?> getById(@PathVariable("id") String id) throws Exception {
-		Asset result = assetService.findById(id);
+		Company result = companyService.findById(id);
 		return new ResponseEntity<>(result, HttpStatus.OK);
 
 	}
 	
 	@PostMapping
-	public ResponseEntity<?> insert(@RequestBody Asset data) throws Exception {
-		assetService.saveOrUpdate(data);
+	public ResponseEntity<?> insert(@RequestBody Company data) throws Exception {
+		companyService.saveOrUpdate(data);
 		return new ResponseEntity<>(data, HttpStatus.OK);
 	}
 
 	@PutMapping
-	public ResponseEntity<?> update(@RequestBody Asset data) throws Exception {
-		assetService.saveOrUpdate(data);
+	public ResponseEntity<?> update(@RequestBody Company data) throws Exception {
+		companyService.saveOrUpdate(data);
 		return new ResponseEntity<>(data, HttpStatus.OK);
 	}
 	
 	@DeleteMapping("{id}")
 	public ResponseEntity<?> delete(@PathVariable("id") String id) throws Exception {
-		boolean result = assetService.removeById(id);
+		boolean result = companyService.removeById(id);
 		
 		return new ResponseEntity<>(result, HttpStatus.OK);
 	}
