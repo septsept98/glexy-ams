fff83483098021fd9a38261c67887f2ac2e9b591
febri228 <febri.ufairahasan20@gmail.com>
2021-12-16 09:32:12 +0700

    asset dao feb

--- glexy/src/main/java/com/lawencon/glexy/dao/AssetDaoImpl.java
+++ glexy/src/main/java/com/lawencon/glexy/dao/AssetDaoImpl.java
@@ -118,86 +118,31 @@ public class AssetDaoImpl extends BaseDaoImpl<Asset> implements AssetDao{
 		}
 		return resultList;
 	}
-
+	
 	@Override
 	public List<Asset> findByCompanyId(String companyId) throws Exception {
-		StringBuilder sb = new StringBuilder();
-		sb.append("SELECT ass.id, ass.names, ass.code, ass.expired_date, ass.asset_img, ass.invoice_id, invo.code, ass.company_id, ");
-		sb.append("cmp.names, ass.inventory_id, inven.code, inven.stock ass.status_asset_id, sa.name_status_asset, ass.brand_id, br.names, ");
-		sb.append("as.asset_type_id, ast.names, ast.code, as.is_active ");
-		sb.append("FROM assets as ");
-		sb.append("INNER JOIN files fl ON fl.id = ass.asset_img ");
-		sb.append("INNER JOIN invoices invo ON invo.id = ass.invoice_id ");
-		sb.append("INNER JOIN companies cmp ON cmp.id = ass.company_id ");
-		sb.append("INNER JOIN inventories inven ON inven.id = ass.inventory_id ");
-		sb.append("INNER JOIN status_assets sa ON sa.id = ass.status_asset_id ");
-		sb.append("INNER JOIN brands br ON br.id = ass.brand_id ");
-		sb.append("INNER JOIN asset_types at ON ast.id = ass.asset_type_id ");
-		sb.append("WHERE ass.company_id = :company_id");
-		String sql = sb.toString();
 		List<Asset> resultList = new ArrayList<>();
+		StringBuilder sb = new StringBuilder();
+		sb.append("SELECT ass ");
+		sb.append("FROM Asset as ass ");
+		sb.append("INNER JOIN FETCH ass.invoiceId ");
+		sb.append("INNER JOIN FETCH ass.companyId as com ");
+		sb.append("INNER JOIN FETCH ass.assetImg ");
+		sb.append("INNER JOIN FETCH ass.assetTypeId ");
+		sb.append("INNER JOIN FETCH ass.inventoryId ");
+		sb.append("INNER JOIN FETCH ass.statusAssetId ");
+		sb.append("WHERE com.id = :com.id ");
 		
-		try {
-			List<?> result = createNativeQuery(sql)
-							.setParameter("company_id", companyId)
-							.getResultList();
-			
-			result.forEach(rs -> {
-				Object[] objArr = (Object[]) rs;
-				Asset asset = new Asset();
-				File file = new File();
-				Invoice invoice = new Invoice();
-				Company company = new Company();
-				Inventory inventory = new Inventory();
-				StatusAsset statusAsset = new StatusAsset();
-				Brand brand = new Brand();
-				AssetType assetType = new AssetType();
-				
-				asset.setId(objArr[0].toString());
-				asset.setNames(objArr[1].toString());
-				asset.setCode(objArr[2].toString());
-				asset.setExpiredDate((LocalDate) objArr[3]);
-				
-				file.setId(objArr[4].toString());
-				asset.setAssetImg(file);
-				
-				invoice.setId(objArr[5].toString());
-				invoice.setCode(objArr[6].toString());
-				asset.setInvoiceId(invoice);
-				
-				company.setId(objArr[7].toString());
-				company.setNames(objArr[87].toString());
-				asset.setCompanyId(company);
-				
-				inventory.setId(objArr[9].toString());
-				inventory.setCode(objArr[10].toString());
-				inventory.setStock(Integer.valueOf(objArr[11].toString()));
-				asset.setInventoryId(inventory);
-				
-				statusAsset.setId(objArr[12].toString());
-				statusAsset.setNameStatusAsset(objArr[13].toString());
-				asset.setStatusAssetId(statusAsset);
-				
-				
-				brand.setId(objArr[14].toString());
-				brand.setNames(objArr[15].toString());
-				asset.setBrandId(brand);
-				
-				assetType.setId(objArr[16].toString());
-				assetType.setNames(objArr[17].toString());
-				assetType.setCode(objArr[18].toString());
-				asset.setAssetTypeId(assetType);
+		String sql = sb.toString();
+		resultList = createQuery(sql, Asset.class)
+				.getResultList();
 
-				asset.setIsActive(Boolean.valueOf(objArr[19].toString()));
-				
-				resultList.add(asset);
-				
-			});
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+		
 		return resultList;
 	}
+	
+
+	
 	
 	
 
