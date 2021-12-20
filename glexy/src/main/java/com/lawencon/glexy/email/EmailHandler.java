44d804c28e1ea62004151af699650ce39ced0523
glenn9828 <glenn9828@gmail.com>
2021-12-20 15:27:19 +0700

    email

--- glexy/src/main/java/com/lawencon/glexy/email/EmailHandler.java
+++ glexy/src/main/java/com/lawencon/glexy/email/EmailHandler.java
@@ -17,7 +17,7 @@ public class EmailHandler {
 	protected JavaMailSender mailSender;
 	
 	@Async
-	public void sendSimpleMessage(String to, String subject, String text) throws Exception{
+	public void sendSimpleMessage(String to, String subject, String header ,String text) throws Exception{
 
 		
 		MimeMessage message = mailSender.createMimeMessage();
@@ -27,7 +27,46 @@ public class EmailHandler {
 	    helper.setFrom("noreply@baeldung.com");
 	    helper.setTo(to);
 	    helper.setSubject(subject);
-	    helper.setText("<b>"+text+"</b>",true);
+	    helper.setText("<!DOCTYPE html>"
+	    		+ "<html>"
+	    		+ "<head>"
+	    		+ "    <style>"
+	    		+ "        .body {\r\n"
+	    		+ "            background-color: #235784;"
+	    		+ "            width: 60vw;"
+	    		+ "            height: 60vh;"
+	    		+ "			   border-radius: 10px;"
+	    		+ "            display: inline-block;"
+	    		+ "            margin-left:  20px;"
+	    		+ "        }"
+	    		+ "        .body__email {"
+	    		+ "            background-color: white;"
+	    		+ "            width: 60%;"
+	    		+ "            height: 60%;"
+	    		+ "            display: inline-block;"
+	    		+ "			   border: 5px #FFCA03 solid;"
+	    		+ "			   border-radius: 10px;"
+	    		+ "            margin-left:  20%;"
+	    		+ "            margin-top:  10%;"
+	    		+ "        }"
+	    		+ "        .body__email h1 {"
+	    		+ "            margin-top: 10%;"
+	    		+ "          text-align: center;"
+	    		+ "        }"
+	    		+ "        .body__email h2 {"
+	    		+ "          text-align: center;"
+	    		+ "        }"
+	    		+ "    </style>"
+	    		+ "</head>"
+	    		+ "<body>"
+	    		+ "    <div class=\"body\">"
+	    		+ "        <div class=\"body__email\">"
+	    		+ "            <h1>"+header+"</h1>"
+	    		+ "            <h2>"+text+"</h2>"
+	    		+ "        </div>"
+	    		+ "    </div>"
+	    		+ "</body>"
+	    		+ "</html>",true);
 	    mailSender.send(message);
 	}
 	
